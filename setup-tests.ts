4c5feb0ccf3dcebed1deeba458b766369d4ee29c
Kyle Fletcher <kfletcher87@gmail.com>
2026-03-05 19:02:16 -0600

    sanity check test setup

--- setup-tests.ts
+++ setup-tests.ts
@@ -1,19 +1,30 @@
 import { setupServer } from 'msw/node';
 import { http, HttpResponse } from 'msw';
 import { afterAll, afterEach, beforeAll } from 'vitest';
+import { REST_COUNTRIES_URL } from './src/api/config';
+import { faker } from '@faker-js/faker';
 
-const allCountriesHandler = http.get('https://restcountries.com/v3.1/all', () => HttpResponse.json([]));
+const allCountriesHandler = http.get(REST_COUNTRIES_URL, () => HttpResponse.json([
+    {
+        name: {
+            common: faker.string.alpha(),
+        },
+        flag: 'not sure',
+        population: 123,
+
+    },
+]));
 
 const server = setupServer(allCountriesHandler);
 
 beforeAll(() => {
-  server.listen();
+    server.listen();
 });
 
 afterEach(() => {
-  server.resetHandlers();
+    server.resetHandlers();
 });
 
 afterAll(() => {
-  server.close();
+    server.close();
 });
---
+++ src/api/config.ts
@@ -0,0 +1,1 @@
+export const REST_COUNTRIES_URL = 'https://restcountries.com/v3.1/all?fields=name,flag,population,region,capital';
